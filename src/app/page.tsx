6f93caaf05bdf581380f7416369a981ad501a1a6
Arshie13 <104509367+Arshie13@users.noreply.github.com>
2025-04-26 11:03:16 +0800

    fix: webview css test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,23 @@
 import { HeroPage } from "@/components/hero-page";
 import Navbar from "@/components/navbar";
-import {AboutUs} from "@/components/aboutUs";
+import { AboutUs } from "@/components/aboutUs";
 import FootNote from "@/components/footer";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="overflow-hidden">
-      <Navbar />
-      <HeroPage />
-      <section
-          id="about">    
-            <AboutUs />
-      </section>
-      <FootNote />
-    </div>
+    <>
+    <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </Head>
+      <div className="overflow-hidden">
+        <Navbar />
+        <HeroPage />
+        <section id="about">
+          <AboutUs />
+        </section>
+        <FootNote />
+      </div>
+    </>
   );
 }
